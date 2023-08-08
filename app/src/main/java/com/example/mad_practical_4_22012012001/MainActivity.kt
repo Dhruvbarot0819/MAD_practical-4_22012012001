735cd92c474aac562f971fc04f6e42f8eacc4348
student <student@gnu.ac.in>
2023-08-08 10:42:01 +0530

    commited

--- app/src/main/java/com/example/mad_practical_4_22012012001/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_4_22012012001/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical_4_22012012001
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,7 +9,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var Login_buttton=findViewById<Button>(R.id.button3)
-        var Signup_button=findViewById<Button>(R.id.button4)
+        var Login_buttton:Button=findViewById<Button>(R.id.login)
+        var Signup_button:Button=findViewById<Button>(R.id.signup)
+
+        Login_buttton.setOnClickListener {
+            Intent(this@MainActivity,LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        Signup_button.setOnClickListener {
+            Intent(this@MainActivity,RegistractionActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
